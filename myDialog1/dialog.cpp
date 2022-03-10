60fda4fed8862fae2871fc57e7b23eab9b1e42eb
Alfredo <alfredo.vargas-paredes@outlook.com>
2022-03-10 23:14:11 +0100

    2022-03-10 23:1411

---
+++ myDialog1/dialog.cpp
@@ -0,0 +1,15 @@
+﻿#include "dialog.h"
+#include <QWidget>
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog{parent},
+    m_label(new QLineEdit)
+{
+    this->setWindowTitle("myDialog1");
+
+}
+
+Dialog::~Dialog()
+{
+}
+
