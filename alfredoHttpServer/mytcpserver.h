4f4ca8542e96ce61e6399591308877eca15dfb77
Alfredo-Vargas <alfredo.vargas-paredes@outlook.com>
2022-05-19 08:33:22 +0200

    alfredo http server start

---
+++ alfredoHttpServer/mytcpserver.h
@@ -0,0 +1,16 @@
+#ifndef MYTCPSERVER_H
+#define MYTCPSERVER_H
+
+#include <QObject>
+
+class MyTcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyTcpServer(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // MYTCPSERVER_H
